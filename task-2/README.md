0e02a6c863623e555e82cc8c17b2590fa5192881
gKonstantin <gkonstantin17@ya.ru>
2025-10-06 22:20:00 +0300

    task-2

---
+++ task-2/README.md
@@ -0,0 +1,4 @@
+# SQL
+
+![1](../img/task2.1.png)
+![1](../img/task2.2.png)
---
+++ task-2/SQLQuery.sql
@@ -0,0 +1,33 @@
+WITH RecursiveSubdivisions AS (
+    SELECT 
+        s.id,
+        s.name,
+        s.parent_id,
+        0 as level
+    FROM subdivisions s
+    WHERE s.id = (SELECT subdivision_id FROM collaborators WHERE id = 710253)
+    
+    UNION ALL
+    
+    -- Recursive: находим все дочерние подразделения
+    SELECT 
+        s.id,
+        s.name,
+        s.parent_id,
+        r.level + 1 as level
+    FROM subdivisions s 
+    INNER JOIN RecursiveSubdivisions r ON s.parent_id = r.id
+    WHERE s.id NOT IN (100055, 100059)
+)
+SELECT 
+    c.id,
+    c.name,
+    s.name as sub_name,
+    c.subdivision_id as sub_id,
+    s.level as sub_level,
+    (SELECT COUNT(*) FROM collaborators c2 WHERE c2.subdivision_id = c.subdivision_id) as colls_count
+FROM collaborators c
+INNER JOIN RecursiveSubdivisions s ON c.subdivision_id = s.id
+WHERE c.age < 40
+  AND s.level > 0
+ORDER BY s.level ASC;
